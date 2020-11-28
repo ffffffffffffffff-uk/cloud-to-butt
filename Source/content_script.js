6e66b9e8123e0c3593021d1e7f0370fff513e97f
ffffffffffffffff-uk <github@ffffffffffffffff.uk>
2020-11-28 12:34:11 +0000

    merged PR from other repo added replace for COVID-19 with trains

--- Source/content_script.js
+++ Source/content_script.js
@@ -1,4 +1,26 @@
+// applied changes from https://github.com/panicsteve/cloud-to-butt/pull/41/commits/2b10bd5e31b81b29b4bbd27f14630a2d4cadbbe8
+//
 walk(document.body);
+
+if (window.MutationObserver) {
+	var observer = new MutationObserver(function (mutations) {
+		Array.prototype.forEach.call(mutations, function (m) {
+			if (m.type === 'childList') {
+				walk(m.target);
+			} else if (m.target.nodeType === 3) {
+				handleText(m.target);
+			}
+		});
+	});
+
+	observer.observe(document.body, {
+		childList: true,
+		attributes: false,
+		characterData: true,
+		subtree: true
+	});
+}
+
 
 function walk(node) 
 {
@@ -37,14 +59,21 @@ function walk(node)
 
 function handleText(textNode) 
 {
-	var v = textNode.nodeValue;
+	var oldValue = textNode.nodeValue;
+	v = oldValue;
 
 	v = v.replace(/\bThe Cloud\b/g, "My Butt");
 	v = v.replace(/\bThe cloud\b/g, "My butt");
 	v = v.replace(/\bthe Cloud\b/g, "my Butt");
 	v = v.replace(/\bthe cloud\b/g, "my butt");
+	v = v.replace(/\bAbout COVID-19\b/gi, function (x) { return x.replace("COVID-19","trains")} );
+
+
+	// avoid infinite series of DOM changes
+	if (v !== oldValue) {
+		textNode.nodeValue = v;
+	}
 	
-	textNode.nodeValue = v;
 }
 
 
